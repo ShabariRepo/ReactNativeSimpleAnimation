efbc37e40ad777296ee6bdfec401c3ec4733359d
shareDemCodez <shabari.cs@gmail.com>
2018-06-05 23:02:06 -0400

    - user drags far enough - make the cards exit - get the next card ready for swiping - default props for re-usable components

--- src/Deck.js
+++ src/Deck.js
@@ -5,8 +5,16 @@ import { View, Animated, PanResponder, Dimensions } from 'react-native';
 const SCREEN_WIDTH = Dimensions.get('window').width;
 // thresh hold for swiping 1/4 of the screen
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+// duration of swipe out card
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+    // if user of the component does not pass in the given props than it will use the ones here as default
+    // always do a default as a practice, when writing re-usable components (to avoid type checking like if(onSwipeRight) do.. and avoid errors)
+    static defaultProps = {
+        onSwipeRight: () => {},
+        onSwipeLeft: () => {}
+    }
 
     constructor(props) {
         super(props);
@@ -33,8 +41,10 @@ class Deck extends Component {
             onPanResponderRelease: (event, gesture) => {
                 if (gesture.dx > SWIPE_THRESHOLD){
                     // swipe right
+                    this.forceSwipe('right');
                 } else if (gesture.dx < -SWIPE_THRESHOLD) {
                     // swipe left (negative to indicate left)
+                    this.forceSwipe('left');
                 } else {
                     this.resetPosition();
                 }
@@ -45,7 +55,18 @@ class Deck extends Component {
         // we will follow this convention but you can easily do just
         ////// this.panResponder = panResponder
         // you can create a panResponder to its own component (its outside of a state system)
-        this.state = { panResponder, position };
+        this.state = { panResponder, position, index: 0 };
+    }
+
+    forceSwipe(direction){
+        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+        // timing is same as spring but timing is more linear
+        // timing is used anytime there needs to be updates to NEW position (difference is you have to specify duration of animation)
+        Animated.timing(this.state.position, {
+            toValue: { x, y: 0 },
+            duration: SWIPE_OUT_DURATION
+        }).start(() => this.onSwipeComplete(direction));
+        // callback once animation is complete and pass direction
     }
 
     // reset the position of the card to default (0, 0)
@@ -53,6 +74,25 @@ class Deck extends Component {
         Animated.spring(this.state.position, {
             toValue: { x:0, y:0 }
         }).start();
+    }
+
+    // after swiped card animation is complete (off the page)
+    onSwipeComplete(direction){
+        const { onSwipeLeft, onSwipeRight, data } = this.props;
+        const item = data[this.state.index];
+
+        direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+
+        // get the next card ready for swiping
+        // reset the position x y
+        // if didnt take the bottom step then when position attached to next card then the next card will go where card before goes
+        // below is weird goes back to the need to not set it in state as the setState isnt called
+        // can do this as just this.position...
+        // if you do this then you have to completely remove position from state (constructor)
+        this.state.position.setValue({ x: 0, y: 0 });
+        // NOT doing just this.state.index ++;
+        // not modifying the existing value just resetting it using setState
+        this.setState({ index: this.state.index + 1 })
     }
 
     // helper method to determine the styling for the top card
